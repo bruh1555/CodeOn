beb71d017bb35e3ca3b157886302b85ff32cd110
bruh1555 <128431763+bruh1555@users.noreply.github.com>
2024-06-15 15:33:30 -0500

    Update gnv.py

--- gnv.py
+++ gnv.py
@@ -1,5 +1,18 @@
+import urllib
+import sys
+import requests
 def gnv():
-    import requests
+    try:
+        url = "https://www.google.com"
+        urllib.urlopen(url)
+        tempstatus = "Connected"
+    except:
+        tempstatus = "Not connected"
+    if not tempstatus == "Connected":
+        print("Error: You are not connected to the internet. CodeOn requires an internet connection. Please try again later when you have one.")
+        return 'Error: No Wifi Connection'
+    else:
+        continue
     response = requests.get("https://raw.githubusercontent.com/bruh1555/CodeOn/main/latest_version.txt")
 
     if response.status_code == 200:
