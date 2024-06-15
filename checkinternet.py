2fa43d7a2bbe37ad60bd89c548be84f585aa58a1
bruh1555 <128431763+bruh1555@users.noreply.github.com>
2024-06-15 16:04:30 -0500

    Update checkinternet.py

--- checkinternet.py
+++ checkinternet.py
@@ -1,12 +1,12 @@
 import requests
+
 def check():
-  'Checks if you have an internet connection'
-  try:
-    requests.get("www.google.com")
-    tempstatus = "True"
-  except:
-    tempstatus = "False"
-  if tempstatus == "True":
-    return "True"
-  else:
-    return "False"
+    'Checks if you have an internet connection'
+    try:
+        response = requests.get("https://www.google.com", timeout=5)
+        if response.status_code == 200:
+            return "True"
+        else:
+            return "False"
+    except requests.exceptions.RequestException:
+        return "False"
