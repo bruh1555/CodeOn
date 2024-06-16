f88b8fb186e77508755282812e2024e72d9c535d
bruh1555 <128431763+bruh1555@users.noreply.github.com>
2024-06-16 10:24:41 -0500

    Update and rename CodeOn for windows.py to CodeOn.py

--- CodeOn.py
+++ CodeOn.py
@@ -4,26 +4,34 @@ import os
 import sys
 import ctypes
 import requests
+from pathlib import Path
 import checkinternet
 import gnv as gnv
-from pathlib import Path
+
+def set_console_title(title):
+    if os.name == 'nt':
+        ctypes.windll.kernel32.SetConsoleTitleW(title)
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 def getlatestversionfunc():
     current_script = Path(__file__).resolve()
     parent_dir = current_script.parent
     target_script_path = None
+    
     for root, dirs, files in os.walk(parent_dir):
         if "updatecon.py" in files:
             target_script_path = Path(root) / "updatecon.py"
-            target_script_path2 = f'"{target_script_path}"'
             break
+    
     if target_script_path:
-        os.system('python3 ' + target_script_path2)
-        print("Welcome back! We are reloading CodeOn to recieve the data retrieved from the updater.")
-        script_path2 = os.path.abspath(__file__)
-        script_path = '"' + script_path2 + '"'
-        os.system("python3 " + script_path)
-        sys.exit()
+        os.system(f'python {target_script_path}')
+        print("Welcome back! We are reloading CodeOn to receive the data retrieved from the updater.")
+        script_path = os.path.abspath(__file__)
+        os.execv(sys.executable, ['python'] + [script_path])
     else:
         print("ACTION REQUIRED: THE FILE 'updatecon.py' CANNOT BE FOUND. PLEASE REDOWNLOAD CODEON TO FIX THIS ISSUE.")
         sys.exit()
@@ -51,43 +59,42 @@ else:
         getlatestversionfunc()
     else:
         time.sleep(3)
-        ctypes.windll.kernel32.SetConsoleTitleW('Command Prompt - CodeOn (python3 windows ver.)')
+        set_console_title('CodeOn Command Line')
         print("Welcome to the CodeOn command line! Type in the command 'help' for help.")
         time.sleep(1)
-        os.system('cls')
+        clear_screen()
         newcodeonversion = "0.3"
 
         def parse(program):
             tokens = program.split()
             if not tokens:
-                return None  # Empty input
+                return None
             command = tokens[0]
             if command == 'createfile':
                 if len(tokens) == 2:
-                    return ('createfile', tokens[1], None)  # Filename only
+                    return ('createfile', tokens[1], None)
                 elif len(tokens) > 2:
-                    return ('createfile', tokens[1], ' '.join(tokens[2:]))  # Filename and content
+                    return ('createfile', tokens[1], ' '.join(tokens[2:]))
                 else:
                     print("Invalid command syntax for createfile")
             elif command == 'createfolder':
                 if len(tokens) == 3:
-                    return ('createfolder', tokens[1], tokens[2])  # Folder name and location
+                    return ('createfolder', tokens[1], tokens[2])
                 else:
                     print("Invalid command syntax for createfolder")
             elif command == 'editfile':
                 if len(tokens) == 2:
-                    return ('editfile', tokens[1])  # Filename
+                    return ('editfile', tokens[1])
                 else:
                     print("Invalid command syntax for editfile")
             else:
                 print("Unknown command: ", command)
             return None
 
-
         def create_file():
-            filepath = input("enter the path of the file to create (don't include quotation marks): ")
-            filename = input("what should the file name be: ")
-            content = input("what should the content be (notice this will only be one line so you must use the editfile command to do multiple lines): ")
+            filepath = input("Enter the path of the file to create (don't include quotation marks): ")
+            filename = input("What should the file name be: ")
+            content = input("What should the content be (notice this will only be one line so you must use the editfile command to do multiple lines): ")
             full_path = os.path.join(filepath, filename)
             with open(full_path, "w") as file:
                 if content is not None:
@@ -136,8 +143,6 @@ else:
                         print("Exiting command line...")
                         break
                     elif program == "help":
-                        print("update notes: added 3 new commands -createfile -createfolder -editfile")
-                        print("in the next update, we will be adding new commands and a way that you can run files made for commands on CodeOn.")
                         print("Commands available:")
                         print("- 'exit': exit the command line")
                         print("- 'help': display this help message")
@@ -180,7 +185,7 @@ else:
                         print("- 'rstcon': reset CodeOn (redownload using update service)")
                         continue
                     elif program == "clear":
-                        os.system('cls')
+                        clear_screen()
                         continue
                     if program == "":
                         continue
@@ -193,24 +198,21 @@ else:
                         folder_path = input("Enter the path where the folder should be created (don't include quotation marks): ")
                         create_folder(folder_path, folder_name)
                     elif program == "reload":
-                        os.system('cls')
-                        script_path2 = os.path.abspath(__file__)
-                        script_path = '"' + script_path2 + '"'
-                        os.system("python3 " + script_path)
-                        sys.exit()
+                        clear_screen()
+                        script_path = os.path.abspath(__file__)
+                        os.execv(sys.executable, ['python'] + [script_path])
                     elif program == "read":
                         current_script = Path(__file__).resolve()
                         parent_dir = current_script.parent
                         target_script_path = None
                         for root, dirs, files in os.walk(parent_dir):
-                            if "CodeOn for windows file runner.py" in files:
-                                target_script_path = Path(root) / "CodeOn for windows file runner.py"
-                                target_script_path2 = f'"{target_script_path}"'
+                            if "CodeOn file runner.py" in files:
+                                target_script_path = Path(root) / "CodeOn file runner.py"
                                 break
                         if target_script_path:
-                            os.system('python3 ' + target_script_path2)
+                            os.system(f'python {target_script_path}')
                         else:
-                            print("ACTION REQUIRED: THE FILE 'updatecon.py' CANNOT BE FOUND. PLEASE REDOWNLOAD CODEON TO FIX THIS ISSUE.")
+                            print("ACTION REQUIRED: THE FILE 'CodeOn file runner.py' CANNOT BE FOUND. PLEASE REDOWNLOAD CODEON TO FIX THIS ISSUE.")
                             sys.exit()
                     elif program == "helpread":
                         print("Commands available for reading a file:")
@@ -221,65 +223,54 @@ else:
                     elif program == "info":
                         print("Info about CodeOn:")
                         print(f'CodeOn {newcodeonversion}')
-                        print("Windows Version")
-                        print("CodeOn was made using python.")
-                        print("If you get any python errors, please report them, by emailing developerv0002@gmail.com and the python error.")
-                        print("You may also submit a pull request on the Github repository you got the software from.")
+                        print("Cross-platform Version")
+                        print("CodeOn was made using Python.")
+                        print("If you get any Python errors, please report them by emailing developerv0002@gmail.com and including the Python error.")
+                        print("You may also submit a pull request on the GitHub repository you got the software from.")
                         print("We will fix it as soon as possible and return a version to fix your error.")
                     elif "print " in program:
                         arguments = program.split()
-                        print(arguments[1])
+                        print(' '.join(arguments[1:]))
                     elif "math " in program:
                         arguments = program.split()
-                        if arguments[1] == "+":
-                            floatednum1 = float(arguments[2])
-                            floatednum2 = float(arguments[3])
-                            output = floatednum1 + floatednum2
-                            print(arguments[2] + " + " + arguments[3] + " = " + str(output))
-                        elif arguments[1] == "-":
-                            floatednum1 = float(arguments[2])
-                            floatednum2 = float(arguments[3])
-                            output = floatednum1 - floatednum2
-                            print(arguments[2] + " - " + arguments[3] + " = " + str(output))
-                        elif arguments[1] == "*":
-                            floatednum1 = float(arguments[2])
-                            floatednum2 = float(arguments[3])
-                            output = floatednum1 * floatednum2
-                            print(arguments[2] + " * " + arguments[3] + " = " + str(output))
-                        elif arguments[1] == "/":
-                            floatednum1 = float(arguments[2])
-                            floatednum2 = float(arguments[3])
-                            output = floatednum1 / floatednum2
-                            print(arguments[2] + " / " + arguments[3] + " = " + str(output))
+                        if len(arguments) == 4:
+                            try:
+                                num1 = float(arguments[2])
+                                num2 = float(arguments[3])
+                                if arguments[1] == "+":
+                                    output = num1 + num2
+                                elif arguments[1] == "-":
+                                    output = num1 - num2
+                                elif arguments[1] == "*":
+                                    output = num1 * num2
+                                elif arguments[1] == "/":
+                                    output = num1 / num2
+                                else:
+                                    raise ValueError("Invalid operator")
+                                print(f"{num1} {arguments[1]} {num2} = {output}")
+                            except ValueError as ve:
+                                print(f"Error: {ve}")
                         else:
-                            print("Traceback: Invalid Arguments. Expected '+', '-', '/', or '*'")
+                            print("Usage: math <operator> <num1> <num2>")
                     elif "variable " in program:
                         arguments = program.split()
                         if not arguments[2] == "math":
                             variables[str(arguments[1])] = str(arguments[2])
                         else:
-                            if arguments[3] == "+":
-                                floatednum1 = float(arguments[5])
-                                floatednum2 = float(arguments[6])
-                                output = floatednum1 + floatednum2
+                            try:
+                                if arguments[3] == "+":
+                                    output = float(arguments[5]) + float(arguments[6])
+                                elif arguments[3] == "-":
+                                    output = float(arguments[5]) - float(arguments[6])
+                                elif arguments[3] == "*":
+                                    output = float(arguments[5]) * float(arguments[6])
+                                elif arguments[3] == "/":
+                                    output = float(arguments[5]) / float(arguments[6])
+                                else:
+                                    output = "math"
                                 variables[str(arguments[2])] = output
-                            elif arguments[3] == "-":
-                                floatednum1 = float(arguments[5])
-                                floatednum2 = float(arguments[6])
-                                output = floatednum1 - floatednum2
-                                variables[str(arguments[2])] = output
-                            elif arguments[3] == "*":
-                                floatednum1 = float(arguments[5])
-                                floatednum2 = float(arguments[6])
-                                output = floatednum1 * floatednum2
-                                variables[str(arguments[2])] = output
-                            elif arguments[3] == "/":
-                                floatednum1 = float(arguments[5])
-                                floatednum2 = float(arguments[6])
-                                output = floatednum1 / floatednum2
-                                variables[str(arguments[2])] = output
-                            else:
-                                variables[str(arguments[1])] = "math"
+                            except ValueError as ve:
+                                print(f"Error: {ve}")
                     elif "os" in program:
                         arguments = program.split()
                         if len(arguments) >= 2:
@@ -288,57 +279,64 @@ else:
                                 os.system(program2)
                             elif arguments[1] == "shutdown":
                                 if len(arguments) == 3:
-                                    if arguments[2] == "r":
-                                        os.system("shutdown -r")
-                                        break
-                                    elif arguments[2] == "s":
-                                        os.system("shutdowm -s")
-                                        break
-                                    elif arguments[2] == "l":
-                                        os.system("shutdown -l")
-                                        break
+                                    if os.name == 'nt':
+                                        if arguments[2] == "r":
+                                            os.system("shutdown -r -t 0")
+                                        elif arguments[2] == "s":
+                                            os.system("shutdown -s -t 0")
+                                        elif arguments[2] == "l":
+                                            os.system("shutdown -l")
+                                    else:
+                                        if arguments[2] == "r":
+                                            os.system("sudo shutdown -r now")
+                                        elif arguments[2] == "s":
+                                            os.system("sudo shutdown -h now")
                                 else:
                                     print("Usage:")
                                     print("s - shutdown the device")
                                     print("r - restart the device")
-                                    print("l - logoff")
+                                    print("l - logoff (Windows only)")
                         else:
                             print("Usage:")
                             print("syscmd - call a command on your system.")
                             print("shutdown <s, r, l> - shutdown your system using a specific method")
                     elif program == "user":
-                        os.system('whoami')
+                        if os.name == 'nt':
+                            os.system('whoami')
+                        else:
+                            os.system('id -un')
                     elif program == "py":
                         os.system("python3")
                     elif "py" in program:
                         program2 = program[3:]
                         os.system(f'python3 {program2}')
                     elif program == "ncon":
-                        script_path2 = os.path.abspath(__file__)
-                        script_path = '"' + script_path2 + '"'
-                        os.system(f'start cmd /K python3 {script_path}')
+                        script_path = os.path.abspath(__file__)
+                        if os.name == 'nt':
+                            os.system(f'start cmd /K python {script_path}')
+                        else:
+                            os.system(f'gnome-terminal -- python {script_path}')
                     elif "cmdtitle" in program:
-                        program2 = program[9:]
-                        ctypes.windll.kernel32.SetConsoleTitleW(f'{program2} - CodeOn (python3 windows ver.)')
+                        if os.name == 'nt':
+                            program2 = program[9:]
+                            set_console_title(f'{program2} - CodeOn')
                     elif program == "vmcon":
-                        script_path2 = os.path.abspath(__file__)
-                        script_path = '"' + script_path2 + '"'
-                        os.system(f'python3 {script_path}')
+                        script_path = os.path.abspath(__file__)
+                        os.system(f'python {script_path}')
                     elif program == "rstcon":
                         getlatestversionfunc()
-                    elif program == "":
-                        continue
                     else:
                         print("Traceback: Unknown Command")
-                    buffer = ctypes.create_unicode_buffer(1024)
-                    ctypes.windll.kernel32.GetConsoleTitleW(buffer, 1024)
-                    current_title = buffer.value
-                    if not current_title.endswith(" - CodeOn (python3 windows ver.)"):
-                        new_title = current_title + " - CodeOn (python3 windows ver.)"
-                        ctypes.windll.kernel32.SetConsoleTitleW(new_title)
-            
+                    
+                    if os.name == 'nt':
+                        buffer = ctypes.create_unicode_buffer(1024)
+                        ctypes.windll.kernel32.GetConsoleTitleW(buffer, 1024)
+                        current_title = buffer.value
+                        if not current_title.endswith(" - CodeOn"):
+                            set_console_title(f'{current_title} - CodeOn')
+
                 except EOFError:
-                    print("Python Error Occured.")
+                    print("Python Error Occurred.")
                     break
 
 if __name__ == "__main__":
