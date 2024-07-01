72170ced2be0dec9505dd2f3a4d0864cd11962f9
bruh1555 <128431763+bruh1555@users.noreply.github.com>
2024-07-01 16:28:32 -0500

    Update gnv.py

--- gnv.py
+++ gnv.py
@@ -5,7 +5,14 @@ def gnv():
     returncheck = checkinternet.check()
     if returncheck == "False":
         print("Error: You are not connected to the internet. CodeOn requires an internet connection. Please try again later when you have one.")
-        sys.exit()
+        print("You may load CodeOn without WiFi.")
+        load = input("Would you like to load CodeOn without your WiFi connection? (y/n): ")
+        if load == "n":
+            sys.exit()
+        else:
+            print("Continuing.")
+            print("You will be asked this again.")
+            return f'Error: No Internet'
     else:
         response = requests.get("https://raw.githubusercontent.com/bruh1555/CodeOn/main/latest_version.txt")
 
